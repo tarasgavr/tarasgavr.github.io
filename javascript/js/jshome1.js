4e123953a9020b853cb3f0ab514415221f844b59
tarasgavrilov <tarasgavrilov@gmail.com>
2016-11-19 14:19:49 +0200

    js added

---
+++ javascript/js/jshome1.js
@@ -0,0 +1,27 @@
+   var x = prompt('enter a number');
+   var n = prompt('enter a grade');
+   function grade(a, b)
+    {
+     var result;
+     if (b == 0) {
+     } else {                         //if b == 0
+       if (b > 0) {
+          result = 1;
+         for (var i = 1; i <= b; i++) {
+            result = result * a;
+         };
+       } else {                      //if b > 0
+         if (b < 0) {
+           result = 1;
+           for (var i = -1; i >= b; i--) {
+             result = result * a;
+           }
+           result = 1 / result;
+          } else {
+           alert('Not supplied!')
+         }
+       }
+     };                             //if b < 0
+     return result;
+    };
+   console.log(grade(x, n));
---
+++ javascript/js/jshome2.js
@@ -0,0 +1,25 @@
+   var arr = new Array(5);
+   var username;
+   var fl;
+   var i;
+   for (i = 0; i<5; i++)
+    {
+     arr[i] = prompt('enter a name');
+    }
+   username = prompt('enter an username');
+   for (i = 0; i<5; i++)
+    {
+     if (arr[i] == username)
+     {
+       fl = true;
+     }
+     else
+     {
+       fl = false;
+     }
+    }
+    if (fl) {
+      alert(username + ', you entered successful');
+    } else {
+      alert('Error! The name ' + username + ' does not exist');
+    }
