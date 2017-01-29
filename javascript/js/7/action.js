8e080f5a4946b2f77350780d8ca7f14c98f6497c
tarasgavrilov <tarasgavrilov@gmail.com>
2017-01-29 17:20:38 +0200

    js7  changed

--- javascript/js/7/action.js
+++ javascript/js/7/action.js
@@ -1,8 +1,11 @@
 $(function() {
-  $(".link").click(function(e) {
-  var index = $(".panes").index(this);
-  e.preventDefault;
-  console.log(index);
-  $("#text" + index).css('display','block');
+  var n = 0;
+  $(".panes").click(function() {
+    var index = $(this).index();
+    $(".text" + n).removeClass('show');
+    $(".text" + n).addClass('hide');
+    $(".text" + index).removeClass('hide');
+    $(".text" + index).addClass('show');
+    n = index;
   });
 });
