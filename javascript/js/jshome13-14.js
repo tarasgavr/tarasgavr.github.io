0af0b44882f7c3ecd3e6aa50ccc4cfa7c2780529
тарас <tarasgavrilov@gmail.com>
2017-02-19 21:04:19 +0200

    js13-14 added

--- javascript/js/jshome13-14.js
+++ javascript/js/jshome13-14.js
@@ -1,34 +1,31 @@
-var div = document.createElement('div');
-div.className = "wrapper";
-var formTest = document.createElement('form');
-formTest.setAttribute('action','#');
-formTest.setAttribute('method','post');
-div.appendChild(formTest);
-var testText = document.createElement('p');
-testText.className = 'title';
-testText.appendChild(document.createTextNode('Тест по программированию'));
-formTest.appendChild(testText);
-var numList = document.createElement('ol');
-formTest.appendChild(numList);
+var div = $('<div></div>');
+div.addClass("wrapper");
+var formTest = $('<form></form>');
+formTest.attr('action','#');
+formTest.attr('method','post');
+div.append(formTest);
+var testText = $('<p></p>');
+testText.addClass('title');
+testText.html('Тест по программированию');
+formTest.append(testText);
+var numList = $('<ol></ol>');
+formTest.append(numList);
 for (var i = 1; i < 4; i++) {
-  var numListItem = document.createElement('li');
-  numListItem.setAttribute('style','padding-top: 10px;')
-  numListItem.appendChild(document.createTextNode('Вопрос №' + i));
+  var numListItem = $('<li></li>');
+  numListItem.html('Вопрос №' + i);
   for (var j = 1; j < 4; j++) {
-    var label = document.createElement('label');
-    var newNode = document.createElement('input');
-    newNode.setAttribute('type','checkbox');
-    newNode.setAttribute('id','variant' + i + j);
-    var text = document.createTextNode('Вариант вопроса №' + j)
-    label.appendChild(text);
-    label.insertBefore(newNode,text);
-    numListItem.appendChild(label);
+    var label = $('<label></label>');
+    var newNode = $('<input></input>');
+    newNode.attr('type','checkbox');
+    newNode.attr('id','variant' + i + j);
+    label.append(newNode);
+    label.html('Вариант вопроса №' + j);
+    numListItem.append(label);
   }
-  numList.appendChild(numListItem);
+  numList.append(numListItem);
 }
-var button = document.createElement('input');
-button.setAttribute('type','submit');
-button.setAttribute('value','Проверить мои результаты');
-formTest.appendChild(button);
-
-document.body.appendChild(div);
+var button = $('<input></input>');
+button.attr('type','submit');
+button.attr('value','Проверить мои результаты');
+formTest.append(button);
+$('body').append(div);
