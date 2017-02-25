2e0c98ff0e9aca90730c7537dcc33aa0530278e5
тарас <tarasgavrilov@gmail.com>
2017-02-25 16:15:17 +0200

    js13-14 changed

--- javascript/js/13/jshome13-14.js
+++ javascript/js/13/jshome13-14.js
@@ -1,49 +1,26 @@
 'use strict'
 $(function() {
-  var hw = $('#home').html();
-  localStorage.setItem("wrapper", '<div class="wrapper">');
-  localStorage.setItem("form", '<form action="action.js" method="post">');
-  localStorage.setItem("text", '<p class="title">Тест по программированию</p><ul>');
+  var testStructure = [
+    '<div class="wrapper">',
+    '<form action="action.js" method="post">',
+    '<p class="title">Тест по программированию</p><ul>',
+  ]
   for (var i = 1; i < 4; i++) {
-    localStorage.setItem("list" + i, '<li>Вопрос №' + i);
+  testStructure[i + 3 * i] = '<li>Вопрос №' + i;
     for (var j = 1; j < 4; j++) {
-      localStorage.setItem("checkbox" + i + j, '<label id="variant' + i + j + '"><input type="checkbox">');
-      localStorage.setItem("label" + i + j, 'Вариант ответа №' + j +'</label></li>');
-      }
+      testStructure[i + 3 * i + j] = '<label id="variant' + i + j + '"><input type="checkbox">Вариант ответа №' + j +'</label></li>';
     }
-  localStorage.setItem("submit", '</ul><input type="submit" id="submit" value="Проверить мои результаты">');
-  localStorage.setItem("endtest", '</form></div>');
-
-  var htmlObj = {
-     homework : [
-      {text:localStorage.wrapper},
-      {text:localStorage.form},
-      {text:localStorage.text},
-      {text:localStorage.getItem("list1")},
-      {text:localStorage.getItem("checkbox11")},
-      {text:localStorage.getItem("label11")},
-      {text:localStorage.getItem("checkbox12")},
-      {text:localStorage.getItem("label12")},
-      {text:localStorage.getItem("checkbox13")},
-      {text:localStorage.getItem("label13")},
-      {text:localStorage.getItem("list2")},
-      {text:localStorage.getItem("checkbox21")},
-      {text:localStorage.getItem("label21")},
-      {text:localStorage.getItem("checkbox22")},
-      {text:localStorage.getItem("label22")},
-      {text:localStorage.getItem("checkbox23")},
-      {text:localStorage.getItem("label23")},
-      {text:localStorage.getItem("list3")},
-      {text:localStorage.getItem("checkbox31")},
-      {text:localStorage.getItem("label31")},
-      {text:localStorage.getItem("checkbox32")},
-      {text:localStorage.getItem("label32")},
-      {text:localStorage.getItem("checkbox33")},
-      {text:localStorage.getItem("label33")},
-      {text:localStorage.submit},
-      {text:localStorage.endtest}
-      ]
   }
+  testStructure[16] = '</ul><input type="submit" id="submit" value="Проверить мои результаты"></form></div>';
+  console.log(testStructure);
+  var testJSON = JSON.stringify(testStructure);
+  localStorage.setItem('myKey',testJSON);
+  var testParse = JSON.parse(localStorage.myKey);
+  console.log(testParse);
+  var hw = $('#home').html();
+  var htmlObj = {
+      homework : testParse
+   }
 
   var result = tmpl(hw, htmlObj);
   $('body').append(result);
