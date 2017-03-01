96b5566fe3ede1cc88f7cc8e72d59e9a17dd0f6e
тарас <tarasgavrilov@gmail.com>
2017-03-01 20:16:58 +0200

    jss15-16 added

---
+++ javascript/js/15/js15.js
@@ -0,0 +1,33 @@
+$(function () {
+
+  $('.search_button').click(function (event) {
+    event.preventDefault();
+    var query = $('.query_field').val();
+    var API_KEY = '4681253-a7427a238f577652160f85a4e';
+    var URL = "https://pixabay.com/api/?key="+API_KEY+"&q="+query;
+    $.getJSON(URL, function(data){
+      console.log(data.hits[0]);
+      if (parseInt(data.totalHits) > 0) {
+        $.each(data.hits, function(i, hit){
+            var list = document.createElement('li');
+            $('#list-links').append(list);
+            var link = document.createElement('a');
+            link.innerHTML = 'Автор данного фото ' + hit.user + '. Ключевые слова к фото ' + hit.tags;
+            list.append(link);
+            var list1 = document.createElement('li');
+            $('#list-images').append(list1);
+            var im = document.createElement('img');
+            im.setAttribute('src',hit.previewUrl);
+            list1.append(im);
+        });
+      } else {
+        var list = document.createElement('li');
+        $('#list-links').append(list);
+        var link = document.createElement('a');
+        link.innerHTML = 'Ключевое слово "' + query + '" не найдено!';
+        list.append(link);
+      }
+    });
+  });
+
+});
