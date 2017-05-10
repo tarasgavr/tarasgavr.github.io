cded09d00b18f185666d971602ef7545274050fc
тарас <tarasgavrilov@gmail.com>
2017-05-10 16:09:41 +0300

    exam

--- javascript/js/exam/exam.js
+++ javascript/js/exam/exam.js
@@ -1,3 +1,13 @@
+if ($(window).width() != 320) {
+  $('.grid').masonry({
+    percentPosition: true,
+    columnWidth: $grid.find('.grid-sizer')[0],
+    itemSelector: '.grid-item'
+  });
+} else {
+  $('.img-partner2').attr('src',"img/URLAUBSGLUCK_Landing_Desktop-partner3.jpg");
+  console.log($('.img-partner2'));
+}
 $(function() {
     $('.jcarousel').jcarousel({
     center: true,
@@ -17,13 +27,4 @@ $(function() {
     $('.jcarousel-next').click(function() {
       $('.jcarousel').jcarousel('scroll', '+=1');
     });
-    $('.grid').masonry({
-      percentPosition: true,
-      columnWidth: $grid.find('.grid-sizer')[0],
-      itemSelector: '.grid-item'
-    });
-    if (window.innerWidth === 320) {
-      document.getElementsByClassName('.img-partner2').setAttribute('src','img/URLAUBSGLUCK_Landing_Desktop-partner3.jpg');
-      document.getElementsByClassName('.img-partner3').setAttribute('src','img/URLAUBSGLUCK_Landing_Desktop-partner2.jpg');
-    }
 });
